4f650f2ad8789ba918345ae5d18b5857ef7f7d8e
Subreme <71085002+subreme@users.noreply.github.com>
2021-04-06 14:56:12 +0200

    Create errors.go
    Added the "Errors" Exercise from https://tour.golang.org/methods/20

---
+++ errors.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintln("cannot Sqrt negative number:", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z, y := x, 0.0
+	for math.Abs(y-z) > 1e-6 {
+		y, z = z, z-(z*z-x)/(2*z)
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
