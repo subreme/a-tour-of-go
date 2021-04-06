e650cb776061918a075f1b8bd374e76e0ac0e5ce
Subreme <71085002+subreme@users.noreply.github.com>
2021-04-06 16:19:33 +0200

    Create images.go
    Added the "Images" Exercise from https://tour.golang.org/methods/25

---
+++ images.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct {
+	Width, Height int
+}
+
+func (img Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, img.Width, img.Height)
+}
+
+func (img Image) At(x, y int) color.Color {
+	v := uint8(x ^ y)
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	m := Image{256, 256}
+	pic.ShowImage(m)
+}
